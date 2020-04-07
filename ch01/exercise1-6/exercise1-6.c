d6aa961a914934eafda718e5ef2c9022455562ff
chenyuxiang0425 <chenyuxiang0425@qq.com>
2020-04-07 21:25:10 +0800

    finish exercise1-6

---
+++ ch01/exercise1-6/exercise1-6.c
@@ -0,0 +1,11 @@
+//
+// Created by Chen on 2020/4/7.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Press a key\n");
+    printf("The expression getchar() != EOF eavaluates to %d\n",getchar()!=EOF);
+    return 0;
+}
